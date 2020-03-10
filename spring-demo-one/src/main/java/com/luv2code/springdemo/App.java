44347cf5b3eeb054006a303b392b9f561da12ffd
dimmxx <dimmxx@gmail.com>
2020-03-10 23:06:20 +0200

    initial commit

---
+++ spring-demo-one/src/main/java/com/luv2code/springdemo/App.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo;
+
+public class App 
+{
+    public static void main( String[] args ) {
+
+        Coach theCoach = new TrackCoach();
+        System.out.println(theCoach.getDailyWorkout());
+
+
+
+
+
+
+
+    }
+}
---
+++ spring-demo-one/src/main/java/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloSpringApp {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyForune());
+    }
+
+
+
+
+
+
+
+
+
+
+}
