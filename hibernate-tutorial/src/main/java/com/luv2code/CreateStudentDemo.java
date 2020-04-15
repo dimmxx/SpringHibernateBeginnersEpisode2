c19c5d4ae730dd2afaf36cead663277126c5cca1
dimmxx <dimmxx@gmail.com>
2020-04-15 21:56:27 +0300

    hibernate

---
+++ hibernate-tutorial/src/main/java/com/luv2code/CreateStudentDemo.java
@@ -0,0 +1,31 @@
+package com.luv2code;
+
+import com.luv2code.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            System.out.println("Creating new student object...");
+            Student student = new Student("Paul", "Wall", "paul@gmail.com");
+            session.beginTransaction();
+            System.out.println("Saving the student...");
+            session.save(student);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
