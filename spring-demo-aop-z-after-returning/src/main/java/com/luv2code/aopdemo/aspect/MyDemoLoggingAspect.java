b37ddb174a5ef7452cc9f2e456170dbc3665df13
dimmxx <dimmxx@gmail.com>
2020-06-29 21:07:30 +0300

    order

--- spring-demo-aop-z-after-returning/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-z-after-returning/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,18 +1,33 @@
 package com.luv2code.aopdemo.aspect;
 
+import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Objects;
 
 @Aspect
 @Component
 @Order(1)
 public class MyDemoLoggingAspect {
+
+    @AfterReturning(pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result")
+    public void afterReturningAccountsAdvice(JoinPoint joinPoint, List<Account> result){
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n===>>>Executing @AfterReturning on method: " + method);
+        System.out.println("\n===>>>Executing @AfterReturning. Result: " + result);
+    }
+
+
+
+
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint joinPoint) {
