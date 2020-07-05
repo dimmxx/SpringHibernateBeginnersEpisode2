a2aea02d85c1ab11daa39750d4c42c75742baf49
dimmxx <dimmxx@gmail.com>
2020-07-05 17:54:12 +0300

    mvc-aop

--- customer-tracker-aop/src/main/java/com/luv2code/aspect/CRMLoggingAspect.java
+++ customer-tracker-aop/src/main/java/com/luv2code/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -12,26 +13,40 @@ import java.util.logging.Logger;
 @Component
 public class CRMLoggingAspect {
 
-    private Logger logger = Logger.getLogger(getClass().getName());
+    private final Logger logger = Logger.getLogger(getClass().getName());
 
     @Pointcut("execution(* com.luv2code.controller.*.*(..))")
-    private void forControllerPackage(){}
+    private void forControllerPackage() {
+    }
 
     @Pointcut("execution(* com.luv2code.service.*.*(..))")
-    private void forServicePackage(){}
+    private void forServicePackage() {
+    }
 
     @Pointcut("execution(* com.luv2code.dao.*.*(..))")
-    private void forDaoPackage(){}
+    private void forDaoPackage() {
+    }
 
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
-    private void forAppFlow(){}
+    private void forAppFlow() {
+    }
 
     @Before("forAppFlow()")
-    public void before(JoinPoint joinPoint){
+    public void before(JoinPoint joinPoint) {
         String method = joinPoint.getSignature().toShortString();
         logger.info("===>>>> in @Before: calling method: " + method);
 
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            logger.info("===>>>> argument: " + arg);
+        }
     }
 
+    @AfterReturning(pointcut = "forAppFlow()", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
 
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("=========>>>>>>>>>> in @AfterReturning: calling method: " + method);
+        logger.info("=========>>>>>>>>>> result: " + result);
+    }
 }
