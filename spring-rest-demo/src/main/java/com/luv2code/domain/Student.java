f5ad252c5bdc0018c59f90bbe8ca102449c51770
dimmxx <dimmxx@gmail.com>
2020-09-09 20:29:03 +0300

    test

--- spring-rest-demo/src/main/java/com/luv2code/domain/Student.java
+++ spring-rest-demo/src/main/java/com/luv2code/domain/Student.java
@@ -1,11 +1,12 @@
 package com.luv2code.domain;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     private String firstName;
--- spring-rest-demo/src/main/java/com/luv2code/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/rest/StudentRestController.java
@@ -5,16 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
-
-    @GetMapping("/student")
-    public Student getStudent(){
-        return new Student("Dima", "Borysov");
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        Student student1 = new Student("Dima", "Borysov");
+        Student student2 = new Student("Olia", "Borysova");
+        List<Student> list = new ArrayList<>();
+        list.add(student1);
+        list.add(student2);
+        return list;
     }
-
-
-
 }
