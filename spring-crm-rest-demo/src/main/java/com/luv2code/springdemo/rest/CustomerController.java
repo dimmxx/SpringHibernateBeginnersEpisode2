c7b68fa5b316e462cf56e1d1cca71a331089d70b
dimmxx <dimmxx@gmail.com>
2020-11-09 19:42:43 +0200

    add

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerController.java
@@ -4,16 +4,13 @@ import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import com.luv2code.springdemo.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
-
 
     private final CustomerService customerService;
 
@@ -25,6 +22,22 @@ public class CustomerController {
     public List<Customer> getCustomers(){
         return customerService.getCustomers();
     }
+
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable Integer customerId){
+        Customer customer = customerService.getCustomer(customerId);
+        if(customer == null){
+            throw new CustomerNotFoundException(String.format("Customer with id %d not found", customerId));
+        }
+        return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer){
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
 
 
 
