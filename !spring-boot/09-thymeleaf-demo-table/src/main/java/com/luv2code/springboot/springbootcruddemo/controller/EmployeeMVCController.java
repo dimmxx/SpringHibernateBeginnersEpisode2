e0ff4946e72dae7b5ce8179732d48007d3fbea41
dimmxx <dimmxx@gmail.com>
2020-12-20 11:19:23 +0200

    thymeleaf-table

--- !spring-boot/09-thymeleaf-demo-table/src/main/java/com/luv2code/springboot/springbootcruddemo/controller/EmployeeMVCController.java
+++ !spring-boot/09-thymeleaf-demo-table/src/main/java/com/luv2code/springboot/springbootcruddemo/controller/EmployeeMVCController.java
@@ -1,17 +1,32 @@
 package com.luv2code.springboot.springbootcruddemo.controller;
 
+import com.luv2code.springboot.springbootcruddemo.entity.Employee;
+import com.luv2code.springboot.springbootcruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class EmployeeMVCController {
 
-    @GetMapping("/employees")
-    public String sayHello(Model model){
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/time")
+    public String sayTime(Model model){
         model.addAttribute("theDate", new Date());
-        return "helloworld";
+        return "time";
+    }
+
+    @GetMapping("/employees")
+    public String getEmployees(Model model){
+        List<Employee> employees = employeeService.findAll();
+        model.addAttribute("employees", employees);
+        return "employees_table";
     }
 }
